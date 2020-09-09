53aa4edad2075f5a30b8ab4f4baeca886580d1ff
Damon Li <bingzhenli@hotmail.com>
2020-09-09 11:41:33 +0800

    Add files via upload

---
+++ create_tflite_com.sh
@@ -0,0 +1,9 @@
+cd /opt/data/private/tongue_recognization_based_on_tensorflow_object_detection_api-master
+
+export PYTHONPATH=$PYTHONPATH:'pwd':'pwd'/slim
+
+python object_detection/export_tflite_ssd_graph.py --pipeline_config_path=saved_model/depth_multiplier_0.6/pipeline.config --trained_checkpoint_prefix=saved_model/depth_multiplier_0.6/model.ckpt-20000 --output_directory=export_tongue_inference_ssdlite_mobilenetv2/depth_multiplier_0.6/ --add_postprocessing_op=true
+
+cd /opt/data/private/tensorflow-master
+
+bazel run -c opt tensorflow/lite/toco:toco -- --input_file=/opt/data/private/tongue_recognization_based_on_tensorflow_object_detection_api-master/export_tongue_inference_ssdlite_mobilenetv2/depth_multiplier_0.6/tflite_graph.pb --output_file=/opt/data/private/tongue_recognization_based_on_tensorflow_object_detection_api-master/export_tongue_inference_ssdlite_mobilenetv2/depth_multiplier_0.6/tongue_detect_0.6.tflite --input_shapes=1,300,300,3 --input_arrays=normalized_input_image_tensor --output_arrays='TFLite_Detection_PostProcess','TFLite_Detection_PostProcess:1','TFLite_Detection_PostProcess:2','TFLite_Detection_PostProcess:3'  --inference_type=FLOAT --allow_custom_ops
